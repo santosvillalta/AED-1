91bb17d80cd55c3ccb0e283400a5e46e0c1cdd28
santosvillalta <santos.villalta@unifesp.br>
2025-12-09 12:44:06 -0300

    Create 1057.c

---
+++ 1057.c
@@ -0,0 +1,24 @@
+
+1057 - Chegando Junto
+Resposta:
+Wrong answer (20%)
+Linguagem:
+C++20 (g++ 11.3.0, -std=c++20 -O2 -lm) [+0s] {beta}
+Tempo:
+0.000s
+Tamanho:
+1,12 KB
+Memória:
+-
+Submissão:
+09/12/2025 12:42:55
+DIFERENÇA
+LINHA LADO
+1057-a.out → 1057-a.sol
+Viewed
+Case 1: 0
+Case 2: 0
+Case 1: 2
+Case 2: 2
+Outras diferenças foram encontradas neste caso de teste!
+Você pode verificar as diferenças que encontramos entre sua saída e a solução esperada! Observe que a difere
